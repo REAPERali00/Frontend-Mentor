8ed68714ff3d50fa164b09c841573bc9e92768e4
Alireza <alireza2000jadidi@gmail.com>
2025-05-04 20:32:33 -0400

    initail commit

---
+++ conference-ticket/src/pages/Form/Form.tsx
@@ -0,0 +1,12 @@
+function Form() {
+  return (
+    <>
+      Your Journey to Coding Conf 2025 Starts Here! Secure your spot at next
+      year's biggest coding conference. Upload Avatar Drag and drop or click to
+      upload Upload your photo (JPG or PNG, max size: 500KB). Full Name Email
+      Address example@email.com GitHub Username @yourusername Generate My Ticket
+    </>
+  );
+}
+
+export default Form;
---
+++ conference-ticket/src/pages/ticket/Ticket.tsx
@@ -0,0 +1,11 @@
+function Ticket() {
+  return (
+    <>
+      Congrats, {name} Your ticket is ready. We've emailed your ticket to{" "}
+      {EmailAddress} and will send updates in the run up to the event. Coding
+      Conf Jan 31, 2025 / Austin, TX
+    </>
+  );
+}
+
+export default Ticket;
