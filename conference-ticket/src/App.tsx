cd40a2bf658afef9e1ce631d1223fedbf97a4d5f
alireza <alireza2000jadidi@gmail.com>
2025-05-06 16:03:13 -0400

    form and app basics for css

--- conference-ticket/src/App.tsx
+++ conference-ticket/src/App.tsx
@@ -1,12 +1,16 @@
 import {} from "react";
 import Form from "./pages/Form/Form";
+import logo from "./assets/images/logo-full.svg";
 import "./App.css";
 
 function App() {
   return (
-    <>
+    <div className="container">
+      <div className="logo flex-center">
+        <img src={logo} alt="logo" />
+      </div>
       <Form />
-    </>
+    </div>
   );
 }
 
--- conference-ticket/src/pages/Form/Form.tsx
+++ conference-ticket/src/pages/Form/Form.tsx
@@ -1,10 +1,17 @@
-function Form() {
+import "./Form.css";
+export default function Form() {
   return (
-    <>
-      <h1>Your Journey to Coding Conf 2025 Starts Here!</h1>
+    <section className="form flex-center flex-col">
+      <h1 className="form-heading">
+        Your Journey to Coding Conf 2025 Starts Here!
+      </h1>
       <p>Secure your spot at next year's biggest coding conference.</p>
-      <form>
+      <form className="flex-col">
         {/* <HandleUpload /> */}
+        <p>
+          Upload Avatar Drag and drop or click to upload Upload your photo (JPG
+          or PNG, max size: 500KB).
+        </p>
         <label>Full Name </label>
         <input type="text" />
         <label>Email Address</label>
@@ -13,10 +20,6 @@ function Form() {
         <input type="text" value="@yourusername" />
         <input type="submit" value="Generate My Ticket" />
       </form>
-      Upload Avatar Drag and drop or click to upload Upload your photo (JPG or
-      PNG, max size: 500KB).
-    </>
+    </section>
   );
 }
-
-export default Form;
