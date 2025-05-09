e1fa7233b65042253805db12502ed7bd6c6d3872
alireza <alireza2000jadidi@gmail.com>
2025-05-09 16:23:20 -0400

    added html and organzied css

--- conference-ticket/src/pages/Ticket/Ticket.tsx
+++ conference-ticket/src/pages/Ticket/Ticket.tsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import ticket_svg from "../../assets/images/pattern-ticket.svg";
 import logo from "../../assets/images/logo-full.svg";
+import avatar from "../../assets/images/image-avatar.jpg";
 import "./Ticket.css";
 
 function Ticket() {
   const [name, setName] = useState("Jonatan Kristof");
+  const [gitId, setGitId] = useState("@jonatankristof0101");
   const [email, setEmail] = useState("jonatan@email.com");
   return (
     <div className="ticket flex-col flex-center">
@@ -16,10 +18,22 @@ function Ticket() {
         We've emailed your ticket to {email} and will send updates in the run up
         to the event.
       </p>
-      <div>
-        <img src={ticket_svg} alt="ticket" />
-        <img src={logo} alt="logo" />
-        Coding Conf Jan 31, 2025 / Austin, TX
+      <div className="ticket-main flex-center">
+        <div className="ticket-left">
+          <div className="ticket-event">
+            <img src={logo} alt="logo" />
+            <p>Jan 31, 2025 / Austin, TX</p>
+          </div>
+
+          <div className="usr-info">
+            <img src={avatar} alt="avatar" />
+            <p>{name}</p>
+            <p>{gitId}</p>
+          </div>
+        </div>
+        <div className="ticket-right">
+          <p>#010609</p>
+        </div>
       </div>
     </div>
   );
