eb6ea93aff37409e1fe6fc7334f838cbc8c65f56
alireza <alireza2000jadidi@gmail.com>
2025-05-09 16:58:49 -0400

    updt

--- conference-ticket/src/pages/Ticket/Ticket.tsx
+++ conference-ticket/src/pages/Ticket/Ticket.tsx
@@ -15,10 +15,12 @@ function Ticket() {
         is ready.
       </h1>
       <p>
-        We've emailed your ticket to {email} and will send updates in the run up
-        to the event.
+        We've emailed your ticket to <br />
+        <span className="orange-txt">{email}</span> and will send updates in the
+        run up to the event.
       </p>
-      <div className="ticket-main flex-center">
+      <div className="ticket-main">
+        <img src={ticket_svg} alt="ticket background" className="ticket-bg" />
         <div className="ticket-left">
           <div className="ticket-event">
             <img src={logo} alt="logo" />
@@ -32,7 +34,7 @@ function Ticket() {
           </div>
         </div>
         <div className="ticket-right">
-          <p>#010609</p>
+          <div className="ticket-code">#010609</div>
         </div>
       </div>
     </div>
